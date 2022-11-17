2a98238bddb0d80fed9de97e72c68b37b148516b
Nishant Choudhary <74289592+nishant998@users.noreply.github.com>
2022-11-17 18:41:56 +0530

    Update Connection.js

--- backend/Dbmanage/Connection.js
+++ backend/Dbmanage/Connection.js
@@ -2,7 +2,7 @@ var AWS = require("aws-sdk") ;
 let awsConfig = {
     "region": "ap-south-1",
     "endpoint": "http://dynamodb.ap-south-1.amazonaws.com",
-    "accessKeyId": "AKIA2KDON5PLJ5IBZFUM", "secretAccessKey":"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"
+    "accessKeyId": "", "secretAccessKey":""
 } ;
 AWS.config.update(awsConfig) ;
 let docClient = new AWS.DynamoDB.DocumentClient() ;
